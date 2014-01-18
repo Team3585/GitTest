ed1ff0402437e9230cb03e779d1a270e54127a6a
Team3585 <roguerobotsof4h@gmail.com>
2014-01-18 08:40:44 -0800

    Created the Netbeans Project
    Prints out "This be a test, arr!"

---
+++ GitTest/src/gittest/GitTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author FRC3585A
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This be a test, arr!");
+    }
+}
